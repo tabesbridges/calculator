ddc50229f9e4be4b6e4b02fce953aedfae158cb7
tabesbridges <tabesbridges@gmail.com>
2022-12-14 19:07:27 -0600

    Add functions to do four basic operations of arithmetic

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+
+function subtractNums(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplyNums(num1, num2) {
+    return num1 * num2;
+}
+
+function addNums(num1, num2) {
+    return num1 / num2;
+}
