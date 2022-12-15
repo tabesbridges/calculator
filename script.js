4c317c15f3cbf5b535e9689cf9ed61f6cbb9fdac
tabesbridges <tabesbridges@gmail.com>
2022-12-14 19:14:33 -0600

    Add operate function

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function multiplyNums(num1, num2) {
     return num1 * num2;
 }
 
-function addNums(num1, num2) {
+function divideNums(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    if (operator = "add") {
+        return addNums(num1, num2);
+    } else if (operator = "subtract") {
+        return subtractNums(num1, num2);
+    } else if (operator = "multiply") {
+        return multiplyNums(num1, num2);
+    } else if (operator = "divide") {
+        return divideNums(num1, num2);
+    }
 }
